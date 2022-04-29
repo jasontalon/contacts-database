e385df597c0c47e7db2d80de6e1c140f7b10a64c
Jason Talon <jason.talon@radix.com.au>
2022-04-29 23:29:43 +0800

    added cookie consent

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -30,6 +30,15 @@ builder.Services.AddIdentityServer()
 builder.Services.AddAuthentication()
     .AddIdentityServerJwt();
 
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential 
+    // cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    // requires using Microsoft.AspNetCore.Http;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
@@ -48,6 +57,7 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCookiePolicy();
 app.UseRouting();
 
 app.UseAuthentication();
