6d9f6bab356303fcd38e3b3c6faf3fca5c8bedc5
Jason Talon <jason.talon@radix.com.au>
2022-05-10 00:15:16 +0800

    dependency injection

---
+++ src/ContactsDatabase.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,69 @@
+using ContactsDatabase.Infrastructure.Identity;
+using ContactsDatabase.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactsDatabase.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+                builder => { builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName); });
+        });
+        
+        serviceCollection.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        serviceCollection.AddIdentityServer()
+            .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+        serviceCollection.AddAuthentication()
+            .AddIdentityServerJwt();
+
+
+        return serviceCollection;
+    }
+
+    public static bool IsDevelopment(this ConfigurationManager configuration) =>  
+        Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")?
+        .Equals("Development", StringComparison.InvariantCultureIgnoreCase) ?? false;
+
+    public static IConfiguration LoadDotEnv(this ConfigurationManager configuration)
+    {
+        var path = Path.Combine(".env");
+
+        for (var level = 0; level <= 3 && !File.Exists(path); level++)
+            path = Path.Combine("../", path);
+
+        var variables = File.ReadAllLines(path)
+            .Where(p => !string.IsNullOrWhiteSpace(p))
+            .Where(p => p.Contains("="))
+            .Where(p => !p.StartsWith("/") || !p.StartsWith("#")).ToList();
+
+        var environmentVariables = variables.ToDictionary(
+            variable => variable.Substring(0, variable.IndexOf("=", StringComparison.Ordinal))
+            , variable =>
+            {
+                var value = variable.Remove(0, variable.IndexOf("=", StringComparison.Ordinal) + 1);
+                if (value.StartsWith("\"") && value.EndsWith("\""))
+                    return value.Remove(0, 1)
+                        .Remove(value.LastIndexOf("\"", StringComparison.Ordinal) - 1,
+                            1); //.Substring(value.LastIndexOf("\""));
+                return value;
+            });
+
+        foreach (var environmentVariable in environmentVariables)
+            Environment.SetEnvironmentVariable(environmentVariable.Key, environmentVariable.Value);
+        
+        configuration.AddEnvironmentVariables();
+        
+        return configuration;
+    }
+}
